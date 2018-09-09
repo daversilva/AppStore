6592e02cd805dd34201f1c802befdf26680445b6
David Rodrigues <davidnpr@hotmail.com>
2018-09-09 15:38:37 -0300

    Criação da tela inicial usando navigation controller.

---
+++ AppStore/FeaturedAppsController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore
+//
+//  Created by David Rodrigues on 09/09/2018.
+//  Copyright © 2018 David Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedAppsController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.white
+    }
+
+}
